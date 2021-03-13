b49740383d22b10b3637e39c6825faf9a4c594e7
Richard Hong <r.hong@pm.me>
2021-03-13 12:35:50 -0500

    update pricing options (#3)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,73 +2,54 @@ import React from "react"
 
 const Hero = () => (
   <div className="container py-12 lg:pb-16">
-    <h2 className="text-3xl sm:text-4xl font-medium leading-tight tracking-tight text-gray-900">
-      3D Virtual Tour Special Offer{" "}
-      <span role="img" aria-label="waving hand">
-        🔥
-      </span>
-      <br />
+    <h2 className="text-4xl sm:text-lg font-medium leading-tight tracking-tight text-gray-900">
+      We do 3D Matterport Virtual Tour with Basic HDR Photos, DSLR Photos, and
+      Drone Photos.
+    </h2>
+    <h2 className="text-xl sm:text-3xl font-medium leading-tight tracking-tight text-gray-900">
+      Pricing for 3D Virtual Tour with Basic HDR Photos:
     </h2>
     <h3 className="text-xl sm:text-2xl font-medium leading-tight tracking-tight text-gray-900 sm:w-4/5 md:w-3/5">
-      <div
-        className="flex flex-row justify-between w-full"
-      >
+      <div className="flex flex-row justify-between w-full">
         <span>Under 1500 Sq. Ft</span>
-        <span className="text-blue-600">
-          $70
-        </span>
+        <span className="text-blue-600">$120</span>
       </div>
-      <div
-        className="flex flex-row justify-between w-full"
-      >
+      <div className="flex flex-row justify-between w-full">
         <span>1500 - 3000 Sq. Ft</span>
-        <span className="text-blue-600">
-          $120
-        </span>
+        <span className="text-blue-600">$150</span>
       </div>
-      <div
-        className="flex flex-row justify-between w-full"
-      >
+      <div className="flex flex-row justify-between w-full">
         <span>3000 - 5000 Sq. Ft</span>
-        <span className="text-blue-600">
-          $180
-        </span>
+        <span className="text-blue-600">$180</span>
       </div>
-      <div
-        className="flex flex-row justify-between w-full"
-      >
+      <div className="flex flex-row justify-between w-full">
         <span>Over 5000 Sq. Ft</span>
-        <span className="text-blue-600">
-          Please Inquire
-        </span>
+        <span className="text-blue-600">Please Inquire</span>
       </div>
     </h3>
     <br />
-    <h3 className="text-xl">
+    <h3 className="text-xl: sm:text-base">
       <span>
         1. The 3D Virtual Tour will be hosted on the Matterport Cloud System for
         $10 / month.
       </span>
-      <br/>
+      <br />
       <span>
         2. All prices include Branded/Unbranded Matterport 3D Virtual Tour Link
         and Up to 40 HDR Photos.
       </span>
-      <br/>
-      <span>
-        3. All prices may be subject to change without notice.
-      </span>
+      <br />
+      <span>3. All prices may be subject to change without notice.</span>
     </h3>
     <br />
     <h3 className="text-2xl sm:text-3xl font-medium leading-tight tracking-tight text-blue-600">
-      Book Now: 
+      Book Now:
     </h3>
     <div className="text-xl leading-tight tracking-tight">
-        (647)-333-7360
+      (647)-333-7360
       <br />
-        3dharbor@gmail.com
+      3dharbor@gmail.com
     </div>
-
   </div>
 )
 
